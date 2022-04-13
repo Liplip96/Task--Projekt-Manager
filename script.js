69e089b4056ef8913ac047a0fa80394839257931
Philip <Luetzenhoff-Philip@web.de>
2022-04-13 15:42:25 +0200

    +

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log(" Hello World");
+
+alert("Das ist eine Warnung");
+
+const divElement = document.getElementsByName("div-element");
+console.log(divElement);
+
+divElement.Element.textContent =" Das ist ein neuer Inhalt";
+
+divElement.classList.add("test-classe");
