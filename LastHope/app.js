e39b22d8b6067d0f663a14e0a7e63a83d2de4511
Philip <Luetzenhoff-Philip@web.de>
2022-05-29 12:27:53 +0200

    saveLists&showPW

--- LastHope/app.js
+++ LastHope/app.js
@@ -1,20 +1,112 @@
-//"use strict";
+const enterbutton = document.getElementById("enter-button");
+const interpretInput = document.getElementById("interpret-input");
+const priceInput = document.getElementById("price-input");
+const dateInput = document.getElementById("datetime-input");
+const tableBody = document.getElementById("table-body");
 
-const todo_input = document.getElementById("todo_input");
-const add_button = document.getElementById("add_button");
-const todo_ul = document.getElementById("todo_ul");
+// Füge einen Eventlistener dem Enter-Button hinzu
+enterbutton.addEventListener("click", enterEvent);
 
-add_button.addEventListener("click", () =>{
-    const input = todo_input.value;
-    const newTodo = document.createElement("li");
-    newTodo.innerText = input;
-    const delete_button = document.createElement("button");
-    delete_button.innerText = "done";
-    newTodo.appendChild(delete_button);
-    todo_ul.appendChild(newTodo);
+/**
+* Eventlistener für die Lösch-Buttons der Tabellen-Einträge
+* @param {Event} evt
+* @param {HTMLElement} parentElement
+*/
+function handleDelete(evt, parentElement) {
+    tableBody.removeChild(parentElement);
+}
 
+/**
+ * Funktion erstellt die Lösch-Button-Zelle
+ * @param {HTMLElement} parentElement
+ * @return {HTMLElement} Gibt die Zelle mit dem Lösch-Button zurück
+ */
+function createDeleteButtonCell(parentElement) {
+    // lege eine neue Tabellenzelle und einen neuen Button an
+    const tablecell = document.createElement("td");
+    const deleteButton = document.createElement("button");
+    // Füllen des Buttons mit Inhalt
+    deleteButton.textContent = "Delete Event";
+    // gib dem Button die passenden HTML-Klassen
+    deleteButton.classList.add("delete-button", "ui", "button", "negative");
+    // Füge einen Eventlistener dem Löschbutton hinzu
+    deleteButton.addEventListener("click", function(evt) {
+        handleDelete(evt, parentElement);
+    });
+    // füge in die Tabellenzelle den Löschbutton ein
+    tablecell.appendChild(deleteButton);
+    // gebe die Tabellenzelle zurück
+    return tablecell;
+}
 
-    delete_button.addEventListener("click",() =>{
-        newTodo.remove();
-    })
-})
+/**
+ * Funktion erstellt eine neuen Event-Eintrag und fügt sie in die Tabelle ein
+ */
+function createNewEventEntry() {
+    // Element für Tabellenreihe anlegen
+    const tableEntry = document.createElement("tr");
+    // Addressierung durch eine entsprechende Klasse
+    tableEntry.classList.add("table-entry");
+    // Auslesen der Input-Werte
+    const interpret = interpretInput.value;
+    const price = Number(priceInput.value);
+    const datetime = dateInput.value;
+    // Validierung der Input-Werte
+    if (!validierung(interpret, price, datetime)) {
+        return;
+    }
+    // lege die einzelnen Tabellenzellen an
+    const cells = [
+        createTableCell(interpret, "data-interpret"),
+        createTableCell(price.toString(), "data-price"),
+        createTableCell(datetime, "data-date"),
+        createDeleteButtonCell(tableEntry)
+    ];
+    // füge die einzelnen Tabellenzellen der Tabellenreihe hinzu
+    for (const cell of cells) {
+        tableEntry.appendChild(cell);
+    }
+    // pflege die gesamte Tabellenreihe in die Tabellenstruktur ein
+    tableBody.appendChild(tableEntry);
+};
+
+/**
+ * Funktion erstellt eine einzelnen Tabellenzelle mit der entsprechenden Werten
+ * @param {String} value
+ * @param {String} name
+ * @return {HTMLElement} Gibt die Tabellenzelle zurück
+ */
+function createTableCell(value, name) {
+    const tablecell = document.createElement("td");
+    tablecell.setAttribute(name, value);
+    tablecell.textContent = value;
+    return tablecell;
+}
+
+/**
+ * Funktion validiert die Eingabefelder "Interpret", "Preis" und "Datum"
+ * @param {String} interpret Der Wert im Interpret-Eingabefeld
+ * @param {Number} price Der Wert im Preis-Eingabefeld
+ * @param {String} datetime Der Wert im Datum-Eingabefeld
+ * @return {Boolean} Gibt true zurück, wenn die Eingaben valide sind
+ */
+function validierung(interpret, price, datetime) {
+    if (!interpret || !price || !datetime) {
+        alert("Please fill out all input fields!");
+        return false;
+    }
+    if (isNaN(price)) {
+        alert("Price Input is not a Number");
+        return false;
+    }
+    return true;
+}
+
+/**
+ * Eventlistener für den Enter-Button
+ * @param {Event} evt
+ */
+function enterEvent(evt) {
+    evt.preventDefault();
+    createNewEventEntry();
+}
--- Project_Taskmanager/script.js
+++ Project_Taskmanager/script.js
@@ -2,21 +2,22 @@
 const todo_input = document.getElementById("todo_input");
 const add_button = document.getElementById("add_button");
 const todo_ul = document.getElementById("todo_ul");
-const name_input = document.getElementById("Notename_input");
-const time_input = document.getElementById("datetime");
+const notename_input = document.getElementById("Notename-input");
+const noteTime_input = document.getElementById("datetime-input");
 
-add_button.addEventListener("click", () =>{
-
+add_button.addEventListener("click", () => {
+    const tInput = noteTime_input.value;
+    const nInput = notename_input.value;
     const input = todo_input.value;
     const newTodo = document.createElement("li");
-    newTodo.innerText = input;
+    newTodo.innerText = tInput + "\n" + nInput + "\n" + input;
     const delete_button = document.createElement("button");
     delete_button.innerText = "done";
     newTodo.appendChild(delete_button);
     todo_ul.appendChild(newTodo);
     todo_input.value = "";
 
-    delete_button.addEventListener("click",() =>{
+    delete_button.addEventListener("click", () => {
         newTodo.remove();
     });
 });
@@ -32,8 +33,16 @@ function passwortSichtbarkeitUmschalten(eingabefeldId) {
     };
 };
 
+/*
 const login_name = document.getElementById(loginName);
 const login_password = document.getElementById("passwort");
+const loginButton = document.getElementById("anmelden");
 
 
+ if (login_name == User || login_password == passwort) {
+        loginButton.href = "ToDo_Notes.html";
 
+    else {
+        alert(wrong password ore username);
+    }
+};*/
